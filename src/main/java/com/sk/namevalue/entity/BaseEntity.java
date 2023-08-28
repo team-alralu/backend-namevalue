a204c1b29f2a834017ed8c739a7e539f3ac9fb78
tlatmsrud <tlatmsrud@naver.com>
2023-08-28 18:27:27 +0900

    :sparkles: Design-BaseEntity

---
+++ src/main/java/com/sk/namevalue/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.sk.namevalue.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * title        : 기본 엔티티
+ * author       : sim
+ * date         : 2023-08-28
+ * description  : 기본 엔티티 클래스
+ */
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+
+}
