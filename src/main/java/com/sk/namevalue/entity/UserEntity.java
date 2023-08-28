29d76246bcce91692e1bd3c46e3b281ede65c99d
tlatmsrud <tlatmsrud@naver.com>
2023-08-28 18:27:17 +0900

    :sparkles: Design-UserEntity

---
+++ src/main/java/com/sk/namevalue/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package com.sk.namevalue.entity;
+
+import com.sk.namevalue.enums.OAuthType;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+/**
+ * title        : 사용자 엔티티
+ * author       : sim
+ * date         : 2023-08-28
+ * description  : 사용자 엔티티
+ */
+
+@Entity
+@Getter
+@Table(name = "tbl_user")
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name ="oauth_type", nullable = false)
+    private OAuthType oauthType;
+}
