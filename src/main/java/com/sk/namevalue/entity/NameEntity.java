122c2ba7eeccc8b3384bf9236e2313db210b2645
tlatmsrud <tlatmsrud@naver.com>
2023-08-28 18:27:50 +0900

    :sparkles: Design-NameEntity

---
+++ src/main/java/com/sk/namevalue/entity/NameEntity.java
@@ -0,0 +1,29 @@
+package com.sk.namevalue.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * title        : 이름 엔티티
+ * author       : sim
+ * date         : 2023-08-28
+ * description  : 사람 이름 엔티티
+ */
+
+@Entity
+@Getter
+@Table(name = "tbl_name")
+public class NameEntity {
+
+    @Id
+    @Column(name = "person_name", nullable = false)
+    private String personName;
+
+    @OneToMany(mappedBy = "personName", cascade = CascadeType.ALL)
+    private List<NamePersonalityEntity> personalityList;
+
+    @OneToMany(mappedBy = "personName", cascade = CascadeType.ALL)
+    private List<NameAnimalEntity> animalList;
+}
