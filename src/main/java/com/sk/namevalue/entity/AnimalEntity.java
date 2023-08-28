c2e73c4e81e92b27eeda892e9cc2fccb00f8e68d
tlatmsrud <tlatmsrud@naver.com>
2023-08-28 18:28:12 +0900

    :sparkles: Design-AnimalEntity

---
+++ src/main/java/com/sk/namevalue/entity/AnimalEntity.java
@@ -0,0 +1,32 @@
+package com.sk.namevalue.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * title        : 동물 엔티티
+ * author       : sim
+ * date         : 2023-08-28
+ * description  : 동물 엔티티 클래스
+ */
+
+@Entity
+@Getter
+@Table(name = "tbl_animal")
+public class AnimalEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "animal_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "emoji", nullable = false)
+    private String emoji;
+
+    @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<NameAnimalEntity> animalEntityList;
+}
