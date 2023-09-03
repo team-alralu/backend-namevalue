438aeadace788e4557871aa95ae4d69d1b9c6c94
tlatmsrud <tlatmsrud@naver.com>
2023-09-03 23:40:04 +0900

    :sparkles: Create-Role-Enum

---
+++ src/main/java/com/sk/namevalue/domain/model/enums/Role.java
@@ -0,0 +1,12 @@
+package com.sk.namevalue.domain.model.enums;
+
+/**
+ * title        : Role
+ * author       : sim
+ * date         : 2023-09-03
+ * description  : 권한 관리 Enum
+ */
+public enum Role {
+
+    ROLE_USER
+}
