38e291c05cb0d36a499ecb102467d753b5cea89e
tlatmsrud <tlatmsrud@naver.com>
2023-09-06 23:58:48 +0900

    :sparkles: Create-test-fixture-management-interface

---
+++ src/test/java/com/sk/namevalue/config/TestFixture.java
@@ -0,0 +1,12 @@
+package com.sk.namevalue.config;
+
+/**
+ * title        : TestFixture
+ * author       : sim
+ * date         : 2023-09-06
+ * description  : Test Fixture 관리 인터페이스
+ */
+public interface TestFixture {
+
+    String VALID_EMAIL = "valid@naver.com";
+}
