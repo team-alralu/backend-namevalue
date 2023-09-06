599a94d611440b8b0cb90f57314a0282fc02c3bf
tlatmsrud <tlatmsrud@naver.com>
2023-09-06 23:51:54 +0900

    :art: Add-static-factory-method

--- src/main/java/com/sk/namevalue/domain/user/domain/UserEntity.java
+++ src/main/java/com/sk/namevalue/domain/user/domain/UserEntity.java
@@ -4,6 +4,9 @@ import com.sk.namevalue.domain.model.BaseEntity;
 import com.sk.namevalue.domain.model.enums.OAuthType;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 /**
  * title        : 사용자 엔티티
@@ -14,6 +17,7 @@ import lombok.Getter;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "tbl_user")
 public class UserEntity extends BaseEntity {
 
@@ -28,5 +32,19 @@ public class UserEntity extends BaseEntity {
     private String name;
 
     @Column(name ="oauth_type", nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private OAuthType oauthType;
+
+    private UserEntity(String email, String name, OAuthType oauthType){
+        this.email = email;
+        this.name = name;
+        this.oauthType = oauthType;
+    }
+
+    public static UserEntity from(Map<String, Object> oAuth2Attributes){
+        String email = (String) oAuth2Attributes.get("email");
+        String name = (String) oAuth2Attributes.get("name");
+        OAuthType oAuthType = OAuthType.valueOf(oAuth2Attributes.get("oauthType").toString());
+        return new UserEntity(email, name, oAuthType);
+    }
 }
