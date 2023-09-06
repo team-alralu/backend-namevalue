63884c7e976e22737b8f21b4f6bc6fb530bcac14
tlatmsrud <tlatmsrud@naver.com>
2023-09-06 23:55:41 +0900

    :sparkles: Create-Authentication-failure-handler
    Oauth2 인증 실패 및 CustomSuccessHandler 에서 예외 발생 시 호출되는 핸들러 클래스

---
+++ src/main/java/com/sk/namevalue/infra/oauth2/CustomFailureHandler.java
@@ -0,0 +1,25 @@
+package com.sk.namevalue.infra.oauth2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+/**
+ * title        : CustomFailureHandler
+ * author       : sim
+ * date         : 2023-09-06
+ * description  : 인증/인가 처리 실패에 대한 핸들러 클래스
+ */
+
+@Slf4j
+public class CustomFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
+        log.error(e.getMessage());
+        response.sendRedirect("/login?status=fail");
+    }
+}
