8efb026c5d26dfe9b87372ed9b4d70ff16fb8601
tlatmsrud <tlatmsrud@naver.com>
2023-09-06 23:58:01 +0900

    :white_check_mark: Test-UserRepository

---
+++ src/test/java/com/sk/namevalue/domain/user/dao/querydsl/UserRepositoryImplTest.java
@@ -0,0 +1,35 @@
+package com.sk.namevalue.domain.user.dao.querydsl;
+
+import com.sk.namevalue.config.TestConfig;
+import com.sk.namevalue.config.TestFixture;
+import com.sk.namevalue.domain.user.dao.UserRepository;
+import com.sk.namevalue.domain.user.domain.UserEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * title        : UserRepositoryImplTest
+ * author       : sim
+ * date         : 2023-09-06
+ * description  : UserRepository QueryDsl 테스트
+ */
+
+@DataJpaTest
+@Import(TestConfig.class)
+class UserRepositoryImplTest implements TestFixture {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저 조회 by email")
+    void findByEmail() {
+        UserEntity userEntity = userRepository.findByEmail(VALID_EMAIL);
+        assertThat(userEntity.getEmail()).isEqualTo(VALID_EMAIL);
+    }
+}
