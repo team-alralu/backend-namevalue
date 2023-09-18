1ceff08f0058629ebd51fedb33669189909379bd
tlatmsrud <tlatmsrud@naver.com>
2023-09-18 15:50:32 +0900

    :sparkles: Create-UserAuthenticationToken
    기존 UsernamePasswordAuthenticationToken 대신 커스텀 Authentication인 UserAuthenticationToken을 사용.
    변경 사유는 UsernamePasswordAuthenticationToken은 폼 인증 시 사용하는 인증 Token이기 때문이며, JWT와는 어울리지 않다고 판단하였음.

---
+++ src/main/java/com/sk/namevalue/infra/oauth2/UserAuthenticationToken.java
@@ -0,0 +1,35 @@
+package com.sk.namevalue.infra.oauth2;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+/**
+ * title        : UserAuthenticationToken
+ * author       : sim
+ * date         : 2023-09-18
+ * description  : UserAuthenticationToken
+ */
+public class UserAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Long principal;
+    private final String credentials;
+
+
+    public UserAuthenticationToken(Long userId, String email, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = userId;
+        this.credentials = email;
+    }
+
+    @Override
+    public String getCredentials() {
+        return credentials;
+    }
+
+    @Override
+    public Long getPrincipal() {
+        return principal;
+    }
+}
