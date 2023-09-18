2af2f3603d903aca12891047d356b01ff5313a1b
tlatmsrud <tlatmsrud@naver.com>
2023-09-18 21:42:08 +0900

    :sparkles: Create-HttpUtilException

---
+++ src/main/java/com/sk/namevalue/global/exception/HttpUtilException.java
@@ -0,0 +1,14 @@
+package com.sk.namevalue.global.exception;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-09-18
+ * description  :
+ */
+public class HttpUtilException extends RuntimeException{
+
+    public HttpUtilException(String message){
+        super(message);
+    }
+}
