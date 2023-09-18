9cdc0e9674ac378c58bd8934490093f5bf72f8e6
tlatmsrud <tlatmsrud@naver.com>
2023-09-18 16:00:27 +0900

    :sparkles: Create-ErrorController

---
+++ src/main/java/com/sk/namevalue/global/controller/ErrorController.java
@@ -0,0 +1,28 @@
+package com.sk.namevalue.global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * title        : Error Controller
+ * author       : sim
+ * date         : 2023-09-18
+ * description  : 에러에 대한 핸들링 컨트롤러
+ */
+
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+
+    @GetMapping("/404")
+    public String page404(){
+        return "/error/404";
+    }
+
+    @GetMapping("/401")
+    public String page401(){
+        return "/error/401";
+    }
+
+}
