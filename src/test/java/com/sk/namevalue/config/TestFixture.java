a5b1c75b729d99a8dbd3dcd097d2a01a7c23c1b6
tlatmsrud <tlatmsrud@naver.com>
2023-09-20 19:10:15 +0900

    :art: Update-Test-Fixture

--- src/test/java/com/sk/namevalue/config/TestFixture.java
+++ src/test/java/com/sk/namevalue/config/TestFixture.java
@@ -1,5 +1,10 @@
 package com.sk.namevalue.config;
 
+import com.sk.namevalue.domain.token.dto.TokenDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.impl.DefaultClaims;
+import java.util.Map;
+
 /**
  * title        : TestFixture
  * author       : sim
@@ -7,6 +12,14 @@ package com.sk.namevalue.config;
  * description  : Test Fixture 관리 인터페이스
  */
 public interface TestFixture {
-
+    Long VALID_USERID = 1L;
     String VALID_EMAIL = "valid@naver.com";
+    String VALID_REFRESH_TOKEN = "VALID_REFRESH_TOKEN";
+    String RENEW_REFRESH_TOKEN = "RENEW_REFRESH_TOKEN";
+    String INVALID_REFRESH_TOKEN = "INVALID_REFRESH_TOKEN";
+    String RENEW_ACCESS_TOKEN = "RENEW_ACCESS_TOKEN";
+    Claims VALID_CLAIMS = new DefaultClaims(Map.of());
+
+    TokenDto RENEW_TOKEN_DTO = new TokenDto(RENEW_ACCESS_TOKEN);
+
 }
