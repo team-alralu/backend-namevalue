f6a8ea5f0ea5a561702eb95c954ba4a02e1b727a
tlatmsrud <tlatmsrud@naver.com>
2023-09-20 19:15:09 +0900

    :sparkles: Create-RedisConfig
    RefreshToken 저장을 위한 Redis 설정 클래스.
    key와 value 모두 String 타입으로 직렬화하여 관리.

---
+++ src/main/java/com/sk/namevalue/global/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package com.sk.namevalue.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * title        : RedisConfig
+ * author       : sim
+ * date         : 2023-09-20
+ * description  : Redis 설정 클래스
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host,port);
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<Long, String> tokenRedisTemplate(){
+
+        RedisTemplate<Long, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
