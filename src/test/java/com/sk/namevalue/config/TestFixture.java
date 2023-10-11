b959c6641cfee16d650f6a8f9be8c3c77a138927
tlatmsrud <tlatmsrud@naver.com>
2023-10-11 21:00:09 +0900

    :recycle: 인터페이스 대신 팩터리 클래스를 사용하라
    테스트에 필요한 데이터를 제공하는 팩터리 클래스로 변경하여 사용한다.
    인터페이스도 가능하긴 하지만, 테스트 데이터를 제공하는 건 인터페이스의 역할과는 거리가 멀기 때문에 클래스로 변경하였다.
    실제로 이펙티브 자바에서는 인터페이스를 통해 상수를 관리하는 것을 안티 패턴으로 분류하고 있다.

--- src/test/java/com/sk/namevalue/config/TestFixture.java
+++ src/test/java/com/sk/namevalue/config/TestFixture.java
@@ -11,15 +11,14 @@ import java.util.Map;
  * date         : 2023-09-06
  * description  : Test Fixture 관리 인터페이스
  */
-public interface TestFixture {
-    Long VALID_USERID = 1L;
-    String VALID_EMAIL = "valid@naver.com";
-    String VALID_REFRESH_TOKEN = "VALID_REFRESH_TOKEN";
-    String RENEW_REFRESH_TOKEN = "RENEW_REFRESH_TOKEN";
-    String INVALID_REFRESH_TOKEN = "INVALID_REFRESH_TOKEN";
-    String RENEW_ACCESS_TOKEN = "RENEW_ACCESS_TOKEN";
-    Claims VALID_CLAIMS = new DefaultClaims(Map.of());
-
-    TokenDto RENEW_TOKEN_DTO = TokenDto.from(RENEW_ACCESS_TOKEN);
+public class TestFixture {
+    public static final Long VALID_USERID = 1L;
+    public static final String VALID_EMAIL = "valid@naver.com";
+    public static final String VALID_REFRESH_TOKEN = "VALID_REFRESH_TOKEN";
+    public static final String RENEW_REFRESH_TOKEN = "RENEW_REFRESH_TOKEN";
+    public static final String INVALID_REFRESH_TOKEN = "INVALID_REFRESH_TOKEN";
+    public static final String RENEW_ACCESS_TOKEN = "RENEW_ACCESS_TOKEN";
+    public static final Claims VALID_CLAIMS = new DefaultClaims(Map.of());
+    public static final TokenDto RENEW_TOKEN_DTO = TokenDto.from(RENEW_ACCESS_TOKEN);
 
 }
