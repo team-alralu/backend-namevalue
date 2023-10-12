e57bd0253d06137a2cc692f160083947be9e47a1
tlatmsrud <tlatmsrud@naver.com>
2023-10-12 20:16:01 +0900

    :sparkles: MockMvc에 대한 팩터리 클래스를 구현하라
    웹 레이어 테스트 코드마다 MockMvc에 대한 구현 코드가 중복되었다. 코드 중복을 막고, 한곳에서 관리하기 위해 MockMvcFactory 클래스를 구현하였다.

---
+++ src/test/java/com/sk/namevalue/config/MockMvcFactory.java
@@ -0,0 +1,26 @@
+package com.sk.namevalue.config;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+
+/**
+ * title        : MockMvc 팩터리 클래스
+ * author       : sim
+ * date         : 2023-10-12
+ * description  : MockMvc 팩터리 클래스
+ */
+public class MockMvcFactory {
+
+    public static MockMvc of(WebApplicationContext webApplicationContext, String charset){
+        return MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(createCharacterEncodingFilter(charset))
+                .build();
+    }
+
+    private static CharacterEncodingFilter createCharacterEncodingFilter(String charset){
+        return new CharacterEncodingFilter(charset);
+    }
+}
