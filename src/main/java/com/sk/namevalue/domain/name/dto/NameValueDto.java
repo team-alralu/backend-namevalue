2c0525cf07f4b32831eb725adbb3d3f8f8f25a96
tlatmsrud <tlatmsrud@naver.com>
2023-10-13 13:51:25 +0900

    :sparkles: 네임벨류 조회 기능에 대한 Dto 클래스를 설계하라

--- src/main/java/com/sk/namevalue/domain/name/dto/NameValueDto.java
+++ src/main/java/com/sk/namevalue/domain/name/dto/NameValueDto.java
@@ -11,25 +11,57 @@ import java.util.List;
  * description  : 네임벨류 Dto
  */
 
-@Getter
+
 public class NameValueDto {
 
-    private final String personName;
-    private final String review;
-    private final List<Long> favoriteList;
-    private final List<Long> animalList;
-    private final List<Long> personalityList;
+    @Getter
+    public static class Save{
+        private final String personName;
+        private final String review;
+        private final List<Long> favoriteList;
+        private final List<Long> animalList;
+        private final List<Long> personalityList;
 
-    private NameValueDto(String personName, String review, List<Long> favoriteList, List<Long> animalList, List<Long> personalityList){
-        this.personName = personName;
-        this.review = review;
-        this.favoriteList = favoriteList;
-        this.animalList = animalList;
-        this.personalityList = personalityList;
+        private Save(String personName, String review, List<Long> favoriteList, List<Long> animalList, List<Long> personalityList){
+            this.personName = personName;
+            this.review = review;
+            this.favoriteList = favoriteList;
+            this.animalList = animalList;
+            this.personalityList = personalityList;
+        }
+
+        public static Save of(String personName, String review, List<Long> favoriteList, List<Long> animalList, List<Long> personalityList) {
+            return new Save(personName, review, favoriteList, animalList, personalityList);
+        }
     }
 
-    public static NameValueDto of(String personName, String review, List<Long> favoriteList, List<Long> animalList, List<Long> personalityList) {
-        return new NameValueDto(personName, review, favoriteList, animalList, personalityList);
+    @Getter
+    public static class Select{
+        private String personName;
+
+        private Select(String personName){
+            this.personName = personName;
+        }
+
+        public static Select of(String personName){
+            return new Select(personName);
+        }
+    }
+
+    @Getter
+    public static class Response{
+
+        private Long reviewId;
+        private String review;
+
+        private Response(Long reviewId, String review){
+            this.reviewId = reviewId;
+            this.review = review;
+        }
+
+        public static Response of(Long reviewId, String review){
+            return new Response(reviewId, review);
+        }
     }
 }
 
