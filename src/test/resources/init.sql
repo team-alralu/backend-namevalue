3ec5e450a5070838422ef54a10781e56a59b22af
tlatmsrud <tlatmsrud@naver.com>
2023-10-13 14:09:54 +0900

    :memo: queryDsl 테스트에 사용될 init 쿼리를 추가하라
    close #10

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -1,1 +1,9 @@
-insert into tbl_user(user_id, email, name, oauth_type, create_date) values (1, 'valid@naver.com', '홍길동', 'NAVER', current_timestamp());
+insert into tbl_user(user_id, email, name, oauth_type, name_value_add_cnt, create_date) values (1, 'valid@naver.com', '홍길동', 'NAVER', 0, current_timestamp());
+
+insert into tbl_person_name(person_name, create_date) values ('홍길동',current_timestamp());
+
+insert into tbl_review (review_id, person_name, content, create_date) values (1L, '홍길동','리뷰입니다. 리뷰입니다.', current_timestamp());
+insert into tbl_review (review_id, person_name, content, create_date) values (2L, '홍길동','리뷰입니다2. 리뷰입니다2.', current_timestamp());
+
+insert into tbl_like(like_id, review_id, user_id, create_date) values (1L, 1L, 1L, current_timestamp());
+insert into tbl_like(like_id, review_id, user_id, create_date) values (1L, 2L, 1L, current_timestamp());
