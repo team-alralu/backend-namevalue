a1f27e375abe0adca85fe92eda85714872188a0a
tlatmsrud <tlatmsrud@naver.com>
2023-10-15 09:36:46 +0900

    :sparkles: MBTI Enum 클래스를 구현하라
    mbti 상수를 Enum 클래스로 관리한다.

---
+++ src/main/java/com/sk/namevalue/domain/model/enums/MBTI.java
@@ -0,0 +1,15 @@
+package com.sk.namevalue.domain.model.enums;
+
+/**
+ * title        : MBTI
+ * author       : sim
+ * date         : 2023-10-15
+ * description  : MBTI
+ */
+public enum MBTI {
+    ISTJ, ISFJ, INFJ, INTJ
+    ,ISTP, ISFP, INFP, INTP
+    ,ESTP, ESFP, ENFP, ENTP
+    ,ESTJ, ESFJ, ENFJ, ENTJ
+    ,NONE
+}
