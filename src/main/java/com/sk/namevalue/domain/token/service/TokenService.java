5bb359d992e83fa0dd758cd36caa37bc57489306
tlatmsrud <tlatmsrud@naver.com>
2023-10-17 02:30:33 +0900

    :recycle: RefreshToken 저장 로직을 메서드로 분리하라
    JWT 토큰을 갱신할 때 뿐 아니라 로그인 성공 시에도 RefreshToken을 저정해야 하기에 메서드로 분리하여 재사용하도록 하였다.

--- src/main/java/com/sk/namevalue/domain/token/service/TokenService.java
+++ src/main/java/com/sk/namevalue/domain/token/service/TokenService.java
@@ -26,19 +26,28 @@ public class TokenService {
 
     /**
      * JWT 토큰 갱신
-     * @param refreshToken - JWT refresh token
+     * @param authorizationValue - Authorization Header 에서 추출한 값
      * @return 갱신된 accessToken 에 대한 TokenDto 인스턴스
      */
-    public TokenDto renewToken(String refreshToken){
+    public TokenDto renewToken(String authorizationValue){
 
+        String refreshToken = jwtProvider.extractJwtToken(authorizationValue);
         Claims claims = jwtProvider.parseJwtToken(refreshToken);
         String renewAccessToken = jwtProvider.generateAccessToken(claims);
         String renewRefreshToken = jwtProvider.generateRefreshToken(claims);
 
         Long userId = jwtProvider.extractIdFromClaims(claims);
-        redisService.save(userId, renewRefreshToken, Token.REFRESH_TOKEN.getMaxAge(), TimeUnit.MINUTES);
-
+        saveRefreshToken(userId, renewRefreshToken);
         return TokenDto.from(renewAccessToken);
+    }
+
+    /**
+     * refresh Token 저장
+     * @param userId - 유저 ID
+     * @param renewRefreshToken - 리프레시 토큰
+     */
+    public void saveRefreshToken(Long userId, String renewRefreshToken){
+        redisService.save(userId, renewRefreshToken, Token.REFRESH_TOKEN.getMaxAge(), TimeUnit.MINUTES);
     }
 
 }
