d386369d32fb2ce7dd6d6bca512bcb33246f69ad
tlatmsrud <tlatmsrud@naver.com>
2023-10-17 01:04:44 +0900

    :sparkles: 이름 정보 조회 및 네임벨류 조회에 대한 컨트롤러 메서드를 구현하라

--- src/main/java/com/sk/namevalue/domain/name/controller/NameValueController.java
+++ src/main/java/com/sk/namevalue/domain/name/controller/NameValueController.java
@@ -1,5 +1,6 @@
 package com.sk.namevalue.domain.name.controller;
 
+import com.sk.namevalue.domain.model.annotation.LoginId;
 import com.sk.namevalue.domain.name.dto.NameValueDto;
 import com.sk.namevalue.domain.name.dto.ValueDto;
 import com.sk.namevalue.domain.name.service.NameValueService;
@@ -27,18 +28,29 @@ public class NameValueController {
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void save(@RequestBody NameValueDto.Save request){
-        nameValueService.save(request);
+    public void save(@LoginId Long userId, @RequestBody NameValueDto.Save request){
+        nameValueService.save(userId, request);
+    }
+
+    /**
+     * 이름 정보 조회
+     * @param request - 요청 Dto
+     * @return - 이름 정보 리스트
+     */
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public NameValueDto.Response getNameInfo(@RequestBody NameValueDto.Select request){
+        return nameValueService.getNameInfo(request);
     }
 
     /**
      * 네임벨류 조회
      * @param request - 요청 Dto
-     * @return - 네임벨류 리스트
+     * @return - 네임벨류
      */
-    @GetMapping
+    @GetMapping("/value")
     @ResponseStatus(HttpStatus.OK)
-    public List<NameValueDto.Response> selectList(@RequestBody NameValueDto.Select request){
-        return nameValueService.selectList(request);
+    public ValueDto.Response getValue(@RequestBody ValueDto.Request request){
+        return nameValueService.getValue(request);
     }
 }
