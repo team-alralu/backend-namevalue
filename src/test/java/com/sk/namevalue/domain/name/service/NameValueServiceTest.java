bf22f1896908170b9cfc82413abe8a6d6eabdd6d
tlatmsrud <tlatmsrud@naver.com>
2023-10-17 00:33:30 +0900

    :white_check_mark: 이름 저장에 대한 서비스 클래스를 테스트하라

--- src/test/java/com/sk/namevalue/domain/name/service/NameValueServiceTest.java
+++ src/test/java/com/sk/namevalue/domain/name/service/NameValueServiceTest.java
@@ -1,48 +1,88 @@
 package com.sk.namevalue.domain.name.service;
 
 import com.sk.namevalue.domain.animal.repository.AnimalRepository;
+import com.sk.namevalue.domain.likeability.repository.LikeabilityRepository;
+import com.sk.namevalue.domain.model.enums.OAuthType;
+import com.sk.namevalue.domain.name.dto.ValueDto;
 import com.sk.namevalue.domain.name.entity.PersonNameEntity;
 import com.sk.namevalue.domain.name.dto.NameValueDto;
 import com.sk.namevalue.domain.name.repository.PersonNameRepository;
 import com.sk.namevalue.domain.personality.repository.PersonalityRepository;
 import com.sk.namevalue.domain.review.repository.ReviewRepository;
+import com.sk.namevalue.domain.user.dao.UserRepository;
+import com.sk.namevalue.domain.user.domain.UserEntity;
+import com.sk.namevalue.global.exception.InvalidUserException;
+import com.sk.namevalue.global.exception.NotProcessException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Optional;
 
 import static com.sk.namevalue.config.fixture.TestFixture.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
- * title        :
+ * title        : NameValueServiceTest
  * author       : sim
  * date         : 2023-10-12
- * description  :
+ * description  : NameValueServiceTest
  */
 class NameValueServiceTest {
 
+    private final UserRepository userRepository = mock(UserRepository.class);
     private final PersonNameRepository personNameRepository = mock(PersonNameRepository.class);
     private final AnimalRepository animalRepository = mock(AnimalRepository.class);
     private final PersonalityRepository personalityRepository = mock(PersonalityRepository.class);
     private final ReviewRepository reviewRepository = mock(ReviewRepository.class);
-    private final NameValueService nameValueService = new NameValueService(personNameRepository, animalRepository,  personalityRepository,reviewRepository);
+    private final LikeabilityRepository likeabilityRepository = mock(LikeabilityRepository.class);
+    private final NameValueService nameValueService = new NameValueService(userRepository, personNameRepository, animalRepository,  personalityRepository,reviewRepository, likeabilityRepository);
 
-    @DisplayName("네임벨류 저장")
+    @DisplayName("유효 사용자에 대한 이름 정보 저장")
     @Test
-    void saveWithValidDto() {
+    void saveWithValidUser() {
+
+        given(userRepository.findById(VALID_USER_ID))
+                .willReturn(Optional.of(VALID_USER_ENTITY));
 
         given(personNameRepository.findById(NAME_VALUE_SAVE_DTO.getPersonName()))
                 .willReturn(Optional.of(PERSON_NAME_ENTITY));
 
-        nameValueService.save(NAME_VALUE_SAVE_DTO);
+        nameValueService.save(VALID_USER_ID, NAME_VALUE_SAVE_DTO);
 
         verify(personNameRepository).save(any(PersonNameEntity.class));
+    }
+
+    @DisplayName("유효하지 않은 사용자에 대한 이름 정보 저장")
+    @Test
+    void saveWithInvalidUser() {
+
+        given(userRepository.findById(INVALID_USER_ID))
+                .willReturn(Optional.empty());
+
+        assertThatThrownBy(() -> nameValueService.save(INVALID_USER_ID, NAME_VALUE_SAVE_DTO))
+                .isInstanceOf(InvalidUserException.class);
+
+        verify(personNameRepository, never()).save(any(PersonNameEntity.class));
+    }
+
+    @DisplayName("이름 등록 후 한시간이 채 지나지 않은 사용자에 대한 이름 정보 저장")
+    @Test
+    void saveWithImpossibleSaveDueToLastRegTime(){
+        UserEntity userEntity = UserEntity.of("valid@naver.com", "홍길동", OAuthType.NAVER);
+        userEntity.renewLastNameRegDate(); // 마지막 이름 등록 일자 갱신
+
+        given(userRepository.findById(VALID_USER_ID))
+                .willReturn(Optional.of(userEntity));
+
+        assertThatThrownBy(() -> nameValueService.save(VALID_USER_ID, NAME_VALUE_SAVE_DTO))
+                .isInstanceOf(NotProcessException.class);
+
+        verify(personNameRepository, never()).save(any(PersonNameEntity.class));
     }
 
     @DisplayName("네임벨류 조회")
