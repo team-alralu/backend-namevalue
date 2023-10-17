841079ec923c7574f99912794ae6da48dd007478
tlatmsrud <tlatmsrud@naver.com>
2023-10-18 04:18:52 +0900

    :sparkles: key를 json 형태로 저장하라
    Long 타입인 사용자 ID 값을 저장 시 StringRedisSerializer 를 사용할 경우 CastException이 발생한다. 때문에 GenericJackson2JsonRedisSerializer를 사용한다.

--- src/main/java/com/sk/namevalue/global/config/RedisConfig.java
+++ src/main/java/com/sk/namevalue/global/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -40,7 +41,7 @@ public class RedisConfig {
 
         RedisTemplate<Long, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
