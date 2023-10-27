dbfe051da13affe1b58f5231e3eae413b31947bc
seungkeungsim <tlatmsrud@naver.com>
2023-10-27 22:42:58 +0900

    :sparkles: restdocs와 연동된 MockMvc를 생성하도록 수정하라

--- src/test/java/com/sk/namevalue/config/MockMvcFactory.java
+++ src/test/java/com/sk/namevalue/config/MockMvcFactory.java
@@ -1,9 +1,12 @@
 package com.sk.namevalue.config;
 
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 
 /**
@@ -14,9 +17,10 @@ import org.springframework.web.filter.CharacterEncodingFilter;
  */
 public class MockMvcFactory {
 
-    public static MockMvc of(WebApplicationContext webApplicationContext, String charset){
+    public static MockMvc of(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation, String charset){
         return MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .addFilter(createCharacterEncodingFilter(charset))
+                .apply(documentationConfiguration(restDocumentation))
                 .build();
     }
 
