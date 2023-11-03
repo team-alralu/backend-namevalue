583f12f49989b49d61872c575e1bb8b499508c8a
tlatmsrud <tlatmsrud@naver.com>
2023-11-03 19:12:07 +0900

    :sparkles: 토큰 갱신에 대한 restdocs snippet 을 생성하라

--- src/test/java/com/sk/namevalue/domain/token/TokenControllerTest.java
+++ src/test/java/com/sk/namevalue/domain/token/TokenControllerTest.java
@@ -8,9 +8,13 @@ import com.sk.namevalue.global.exception.JwtTokenException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -19,6 +23,7 @@ import java.nio.charset.StandardCharsets;
 import static com.sk.namevalue.config.fixture.TestFixture.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(TokenController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@ExtendWith(RestDocumentationExtension.class)
 class TokenControllerTest {
 
     @MockBean
@@ -39,8 +45,8 @@ class TokenControllerTest {
     private MockMvc mockMvc;
     private static final String BEARER = "bearer ";
     @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext){
-        this.mockMvc = MockMvcFactory.of(webApplicationContext, StandardCharsets.UTF_8.name());
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation){
+        this.mockMvc = MockMvcFactory.of(webApplicationContext, restDocumentation, StandardCharsets.UTF_8.name());
     }
 
     @DisplayName("유효 Refresh Token을 통한 AccessToken 갱신")
@@ -52,9 +58,11 @@ class TokenControllerTest {
 
         mockMvc.perform(
                 post("/api/token")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .header("Authorization", BEARER + VALID_REFRESH_TOKEN))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("renewAccessToken"));
     }
 
     @DisplayName("유효하지 않은 Refresh Token을 통한 AccessToken 갱신")
