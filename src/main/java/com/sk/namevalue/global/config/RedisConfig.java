91708b5d0904cf4d6f3fd7810988ac9f67bed383
seungkeungsim <tlatmsrud@naver.com>
2023-12-09 00:49:38 +0900

    :pencil2: Fix

--- src/main/java/com/sk/namevalue/global/config/RedisConfig.java
+++ src/main/java/com/sk/namevalue/global/config/RedisConfig.java
@@ -26,13 +26,9 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.password}")
-    private String password;
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host,port);
-        config.setPassword(password);
         return new LettuceConnectionFactory(config);
     }
 
