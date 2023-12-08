c30c4e0c46178d0bdc06ee881b0499fda863a7b5
seungkeungsim <tlatmsrud@naver.com>
2023-12-08 18:29:30 +0900

    :sparkles: Add-deploy-shell

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "> now running app find!"
+CURRENT_PID=$(pgrep -f backend-namevalue)
+
+echo "$CURRENT_PID"
+if [ -z $CURRENT_PID ]; then
+        echo "> no running app."
+else
+        echo "> kill -9 $CURRENT_PID”
+        kill -9 $CURRENT_PID
+        sleep 3
+fi
+
+echo "> new app deploy"
+
+JAR_NAME=$(ls |grep 'backend-namevalue' | tail -n 1)
+echo "> JAR Name: $JAR_NAME"
+
+echo "> run java"
+nohup java -jar $JAR_NAME > /dev/null 2>&1 &
